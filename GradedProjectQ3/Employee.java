11db09a5cee7fee37105588e8d5920739d3dfcf2
Hari Prasad Korubilli <116051988+hariprasadkorubilli@users.noreply.github.com>
2023-12-10 23:57:23 +0530

    Add files via upload

---
+++ GradedProjectQ3/Employee.java
@@ -0,0 +1,15 @@
+
+public class Employee {
+	public Employee(String empid, String name, String phno, String addr, int sal) {
+		// TODO Auto-generated constructor stub
+	}
+	//public Employee(String string, String string2, String string3, String string4, int i) {
+		// TODO Auto-generated constructor stub
+//	}
+	 //public int ID;
+	// public String name;
+	// public int phno;
+	// public String add;
+	 //public 	int sal;
+
+}
---
+++ GradedProjectQ3/Project.java
@@ -0,0 +1,9 @@
+
+public class Project {
+	
+	public Project(String string, String string2, int i) {
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
